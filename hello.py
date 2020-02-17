402001c41d8946ae5acdcc244ba1da5a46264c2b
alireza.khalesi <alireza.khalesi@peykasa.ir>
2020-02-17 09:44:54 +0330

    add hello world to project

---
+++ hello.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+class Person:
+
+    def __init__(self, name):
+        self.name = name
+
+    def intro(self):
+        print("Hello, Im {0}".format(self.name))
+
+    def __str__(self):
+        return self.name
+
+
+if __name__ == "__main__":
+
+    person1 = Person('alireza')
+    print(person1)
